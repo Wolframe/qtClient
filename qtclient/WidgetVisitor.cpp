adf9f7d990b5d65a89254798c6b0933dc3313298
Andreas Baumann <abaumann@yahoo.com>
2013-07-17 08:43:22 +0200

    reverted all fixes needed for tab widgets

--- qtclient/WidgetVisitor.cpp
+++ qtclient/WidgetVisitor.cpp
@@ -233,16 +233,10 @@ static void getWidgetChildren_( QList<QWidget*>& rt, QObject* wdg)
 {
 	static const QString str_QWidget("QWidget");
 
-#ifdef WOLFRAME_LOWLEVEL_DEBUG
-	qDebug() << "Checking children of type" << wdg->metaObject()->className() << ", name" << wdg->objectName( );
-#endif
 	foreach (QObject* cld, wdg->children())
 	{
 		if (qobject_cast<QLayout*>( cld))
 		{
-#ifdef WOLFRAME_LOWLEVEL_DEBUG
-			qDebug() << "Skipping layout" << cld->objectName() << ", continue search for children";
-#endif
 			getWidgetChildren_( rt, cld);
 		}
 		else
@@ -250,27 +244,12 @@ static void getWidgetChildren_( QList<QWidget*>& rt, QObject* wdg)
 			QWidget* we = qobject_cast<QWidget*>( cld);
 			if (we)
 			{
-/* Aba, wrong IHMO:
 				if (we->layout() && cld->metaObject()->className() == str_QWidget)
 				{
-#ifdef WOLFRAME_LOWLEVEL_DEBUG
-					qDebug() << "Skipping QWidget having a layout, name" << cld->objectName();
-#endif
-					getWidgetChildren_( rt, cld);
-				}
-				else */
-				if( cld->objectName().startsWith( "qt_" ) )
-				{
-#ifdef WOLFRAME_LOWLEVEL_DEBUG
-					qDebug() << "Skipping internal Qt object of type" << cld->metaObject()->className() << ", name" << cld->objectName();
-#endif
 					getWidgetChildren_( rt, cld);
 				}
 				else
 				{
-#ifdef WOLFRAME_LOWLEVEL_DEBUG
-					qDebug() << "Found child of type" << cld->metaObject()->className() << ", name" << cld->objectName();
-#endif
 					rt.push_back( we);
 				}
 			}
@@ -479,7 +458,7 @@ bool WidgetVisitor::enter( const QString& name, bool writemode, int level)
 			QList<QWidget*> cn = children( name);
 			if (cn.size() > 1)
 			{
-				ERROR( "ambiguous widget reference", name);
+				ERROR( "ambiguus widget reference", name);
 				return false;
 			}
 			if (cn.isEmpty()) return false;
