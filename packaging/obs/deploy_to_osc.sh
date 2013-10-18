0e7c224f9ca5f34bd4238866051a81864e972d56
Andreas Baumann <abaumann@yahoo.com>
2013-10-18 14:59:06 +0200

    moved packaging things to sudirectory

--- packaging/obs/deploy_to_osc.sh
+++ packaging/obs/deploy_to_osc.sh
@@ -16,16 +16,16 @@ mv /tmp/wolfclient-$VERSION.tar.gz .
 rm -rf /tmp/wolfclient-$VERSION
 
 cp wolfclient-$VERSION.tar.gz $OSC_HOME/wolfclient_$VERSION.tar.gz
-cp redhat/wolfclient.spec $OSC_HOME/wolfclient.spec
+cp packaging/redhat/wolfclient.spec $OSC_HOME/wolfclient.spec
 
 SIZE=`stat -c '%s' $OSC_HOME/wolfclient_$VERSION.tar.gz`
 CHKSUM=`md5sum $OSC_HOME/wolfclient_$VERSION.tar.gz | cut -f 1 -d' '`
 
 
-cat contrib/osc/wolfclient.dsc > $OSC_HOME/wolfclient.dsc
+cat packaging/obs/wolfclient.dsc > $OSC_HOME/wolfclient.dsc
 echo " $CHKSUM $SIZE wolfclient_$VERSION.tar.gz" >> $OSC_HOME/wolfclient.dsc
 
-cat contrib/osc/PKGBUILD > $OSC_HOME/PKGBUILD
+cat packging/obs/PKGBUILD > $OSC_HOME/PKGBUILD
 echo "md5sums=('$CHKSUM' '$CHKSUM2' '$CHKSUM3')" >> $OSC_HOME/PKGBUILD
 
 # the revision of the git branch we are currently building (master hash at the moment)
