112baf8d527329d3b296daeac4e111ee5a74c6cb
Andreas Baumann <abaumann@yahoo.com>
2013-10-24 22:12:39 +0200

    debug window, made the last column fit the window

--- wolfclient/debugview/DebugViewWidget.cpp
+++ wolfclient/debugview/DebugViewWidget.cpp
@@ -34,6 +34,7 @@
 #include "debugview/DebugLogTree.hpp"
 #include <QPushButton>
 #include <QHBoxLayout>
+#include <QHeaderView>
 
 DebugViewWidget::DebugViewWidget( QWidget *parent_)
 	:QWidget( parent_, Qt::Tool | Qt::WindowTitleHint)
@@ -230,6 +231,7 @@ void DebugViewWidget::printMessages( int nodeid)
 	for (int ii = 0; ii < m_msglist->rowCount(); ii++) {
 		m_msglist->resizeRowToContents( ii);
 	}
+	m_msglist->horizontalHeader()->setStretchLastSection( true);
 	m_msglist->setShowGrid( false);
 }
 
